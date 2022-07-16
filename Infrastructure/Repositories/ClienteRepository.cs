14ef7076609fa95b47ce7f561fc0834d5be480ae
XAlisonCruzX <alisonofdark@gmail.com>
2022-07-16 16:19:32 -0300

    fix: remove try catch

--- Infrastructure/Repositories/ClienteRepository.cs
+++ Infrastructure/Repositories/ClienteRepository.cs
@@ -12,112 +12,71 @@ namespace api_clientes.Repositories
     {
         public IConexao Conexao;
 
-        //Ao instanciar recebe conexao com o DB
         public ClienteRepository(IConexao Conexao)
         {
             this.Conexao = Conexao;
         }
 
-        // Insere os dados do cliente
         public int Add(ClienteModel cliente)
         {
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
+                var resposta = cn.ExecuteScalar<int>(@"INSERT INTO CLIENTES (NOME, DATA_NASCIMENTO, CPF, RG) 
+                            OUTPUT Inserted.ID
+                            VALUES (@NOME, @DATA_NASCIMENTO, @CPF, @RG)", new
                 {
-                    // retorna o id do cliente
-                    var resposta = cn.ExecuteScalar<int>(@"INSERT INTO CLIENTES (NOME, DATA_NASCIMENTO, CPF, RG) 
-                                OUTPUT Inserted.ID
-                                VALUES (@NOME, @DATA_NASCIMENTO, @CPF, @RG)", new
-                    {
-                        cliente.NOME,
-                        cliente.DATA_NASCIMENTO,
-                        cliente.CPF,
-                        cliente.RG
-                    });
+                    cliente.NOME,
+                    cliente.DATA_NASCIMENTO,
+                    cliente.CPF,
+                    cliente.RG
+                });
 
-                    return resposta;
-                }
-            }
-            catch (Exception)
-            {
-                throw;
+                return resposta;
             }
         }
 
         // Deleta clietne pelo id
         public int Delete(int id)
         {
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
-                {
-                    var resposta = cn.Execute(@"DELETE FROM CLIENTES WHERE ID = @id", new { id });
-                    return resposta;
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
+                var resposta = cn.Execute(@"DELETE FROM CLIENTES WHERE ID = @id", new { id });
+                return resposta;
+            }    
         }
 
 
         // Retorna cliente pelo id
         public ClienteModel Get(int id)
         {
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
-                {
-                    var resposta = cn.Query<ClienteModel>(@"SELECT * FROM CLIENTES WHERE ID = @id", new { id });
+                var resposta = cn.Query<ClienteModel>(@"SELECT * FROM CLIENTES WHERE ID = @id", new { id });
 
-                    return resposta.FirstOrDefault();
-                }
-            }
-            catch (Exception)
-            {
-                throw;
+                return resposta.FirstOrDefault();
             }
         }
 
         // Lista todos clientes
         public List<ClienteModel> GetAll()
         {
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
-                {
-                    var resposta = cn.Query<ClienteModel>(@"SELECT * FROM CLIENTES");
+                var resposta = cn.Query<ClienteModel>(@"SELECT * FROM CLIENTES");
 
-                    return resposta.ToList();
-                }
-            }
-            catch (Exception)
-            {
-                throw;
+                return resposta.ToList();
             }
         }
 
         // lista todos cliente com filtro no nome
         public List<ClienteModel> GetAll(string nome)
         {
-
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
-                {
-                    var resposta = cn.Query<ClienteModel>(@"SELECT * FROM CLIENTES WHERE NOME LIKE @nome", new { nome = "%" + nome + "%" });
+                var resposta = cn.Query<ClienteModel>(@"SELECT * FROM CLIENTES WHERE NOME LIKE @nome", new { nome = "%" + nome + "%" });
 
-                    return resposta.ToList();
-                }
+                return resposta.ToList();
             }
-            catch (Exception)
-            {
-                throw;
-            }
-
         }
 
 
@@ -125,25 +84,18 @@ namespace api_clientes.Repositories
         // Atualiza dados do cliente pelo id
         public int Update(ClienteModel cliente, int id)
         {
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
+                var resposta = cn.ExecuteScalar<int>(@"UPDATE CLIENTES SET NOME = @NOME, DATA_NASCIMENTO = @DATA_NASCIMENTO, CPF = @CPF , RG = @RG WHERE ID = @id  ", new
                 {
-                    var resposta = cn.ExecuteScalar<int>(@"UPDATE CLIENTES SET NOME = @NOME, DATA_NASCIMENTO = @DATA_NASCIMENTO, CPF = @CPF , RG = @RG WHERE ID = @id  ", new
-                    {
-                        id,
-                        cliente.NOME,
-                        cliente.DATA_NASCIMENTO,
-                        cliente.CPF,
-                        cliente.RG
-                    });
+                    id,
+                    cliente.NOME,
+                    cliente.DATA_NASCIMENTO,
+                    cliente.CPF,
+                    cliente.RG
+                });
 
-                    return resposta;
-                }
-            }
-            catch (Exception)
-            {
-                throw;
+                return resposta;
             }
         }
 
@@ -152,24 +104,15 @@ namespace api_clientes.Repositories
         // quant = quantidade de registros a buscar
         public List<ClienteModel> GetPag(int offset, int quant)
         {
-
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
+                var resposta = cn.Query<ClienteModel>(@"Select * from CLIENTES order by id OFFSET @offset ROWS FETCH NEXT @quant ROWS ONLY", new
                 {
-                    var resposta = cn.Query<ClienteModel>(@"Select * from CLIENTES order by id OFFSET @offset ROWS FETCH NEXT @quant ROWS ONLY", new
-                    {
-                        offset,
-                        quant
+                    offset,
+                    quant
 
-                    });
-                    return resposta.ToList();
-                }
-
-            }
-            catch (Exception)
-            {
-                throw;
+                });
+                return resposta.ToList();
             }
         }
 
@@ -179,25 +122,16 @@ namespace api_clientes.Repositories
         // nome = filtro
         public List<ClienteModel> GetPag(int offset, int quant, string nome)
         {
-
-            try
+            using (var cn = Conexao.AbrirConexao())
             {
-                using (var cn = Conexao.AbrirConexao())
+                var resposta = cn.Query<ClienteModel>(@"Select * from CLIENTES WHERE NOME LIKE @nome ORDER BY ID OFFSET @offset ROWS FETCH NEXT @quant ROWS ONLY", new
                 {
-                    var resposta = cn.Query<ClienteModel>(@"Select * from CLIENTES WHERE NOME LIKE @nome ORDER BY ID OFFSET @offset ROWS FETCH NEXT @quant ROWS ONLY", new
-                    {
-                        nome = "%" + nome + "%",
-                        offset,
-                        quant
+                    nome = "%" + nome + "%",
+                    offset,
+                    quant
 
-                    });
-                    return resposta.ToList();
-                }
-
-            }
-            catch (Exception)
-            {
-                throw;
+                });
+                return resposta.ToList();
             }
         }
 
